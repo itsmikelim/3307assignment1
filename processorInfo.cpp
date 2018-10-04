806f229bac18f60df88fe41f356c3f08201e7864
Mike Lim <mikelim@mikejr.local>
2018-10-04 17:01:56 -0400

    syntax

--- processorInfo.cpp
+++ processorInfo.cpp
@@ -20,7 +20,7 @@ void processorInfo::getVendor()
     while(getline(finfo,line))
     {
         stringstream str(line);
-        if (getline(str, type, ':' ) && getline(str,info) && type.substr(0,10) == "vendor_id") {
+        if (getline(str, type, ':' ) && getline(str,info) && type.substr(0,10) == "model name") {
             cout << info << endl;
             break;
         }
