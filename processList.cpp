87c94d21c662d465de87e1f0d45ab73d3ba6a0d2
Mike Lim <mikelim@mikejr.local>
2018-10-05 01:14:58 -0400

    syntax

--- processList.cpp
+++ processList.cpp
@@ -27,11 +27,9 @@ int main(void)
         while ((dir = readdir(d)) != NULL)
         {
             i++;
-            if (is_integer(dir))
-            {
 
                 fileList.push_back(dir->d_name);
-            }
+
         }
       //  for(int i=0;i<fileList.size();i++) {
           //  cout<<fileList[i]<<endl;
