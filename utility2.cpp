fb829894953dfc4c352694beadbb8865c48eb3ff
Mike Lim <mikelim@mikejr.local>
2018-10-04 23:18:55 -0400

    syntax

---
+++ utility2.cpp
@@ -0,0 +1,54 @@
+//
+//  utility2.cpp
+//  CS 3307 Assignment 1
+//
+//  Created by Mike Lim on 2018-10-04.
+//
+
+#include "utility2.hpp"
+#include "systemHostName.cpp"
+#include "operatingSystemDetails.cpp"
+#include "processorInfo.cpp"
+#include "memoryInfo.cpp"
+//#include "networkInterfaceList.cpp"
+//#include "networkInterface.cpp"
+//#include "process.cpp"
+//#include "processList.cpp"
+
+
+int main()
+{
+    string response;
+    
+    while (response != exit) {
+        cout << "Would you like to know the information of hostname, operating system, processor, memory, network interfaces, process, or exit?";
+    
+        getline(cin, response);
+    
+        switch(response) {
+            case hostname: systemHostName shn;
+                shn.getHostName();
+                break;
+            case operating system: operatingSystemDetails osd;
+                osd.OSName();
+                osd.systemRelease();
+                osd.systemVersion();
+                osd.machineData();
+                osd.systemUptime();
+                break;
+            case processor: processorInfo PI;
+                PI.getVendor();
+                PI.getModelName();
+                PI.getLoadAverages();
+                break;
+            case memory: memoryInfo mI;
+                mI.totalMemory();
+                mI.freeMemory();
+                break;
+                //case network interface:
+                //case process:
+    
+        }
+    }
+    return 0
+}
---
+++ utility2.hpp
@@ -0,0 +1,13 @@
+//
+//  utility2.hpp
+//  CS 3307 Assignment 1
+//
+//  Created by Mike Lim on 2018-10-04.
+//
+
+#ifndef utility2_hpp
+#define utility2_hpp
+
+#include <stdio.h>
+
+#endif /* utility2_hpp */
